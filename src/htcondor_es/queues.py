02d8c46e9c8fe55cd79b0b462ec0b2e38c3094ce
Benjamin Stieger <stiegerb@cern.ch>
2018-03-06 22:26:05 +0000

    Commenting out the limit on concurrent uploads for now

--- src/htcondor_es/queues.py
+++ src/htcondor_es/queues.py
@@ -254,13 +254,13 @@ def process_queues(schedd_ads, starttime, pool, args):
                                              args=(idx, es_bunch, args))
             futures.append(("UPLOADER_ES", future))
 
-        # Limit the number of concurrent upload processes
-        while len([f for f in futures if f[0].startswith('UPLOADER') and not f[1].ready()]) >= args.upload_pool_size:
-            if time_remaining(starttime) < 0:
-                break
+        # # Limit the number of concurrent upload processes
+        # while len([f for f in futures if f[0].startswith('UPLOADER') and not f[1].ready()]) >= args.upload_pool_size:
+        #     if time_remaining(starttime) < 0:
+        #         break
 
-            # Wait a short time before counting again
-            time.sleep(1)
+        #     # Wait a short time before counting again
+        #     time.sleep(1)
 
     listener.join()
 
